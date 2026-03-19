a0be1c8f564e92837e7242d9746c502d06712a2e
aswin <nuzhumaki9@gmail.com>
2026-03-19 23:56:04 +0530

    refactor(application): change name of userMapper

--- src/application/mappers/userMapper.ts
+++ src/application/mappers/userMapper.ts
@@ -1,6 +1,6 @@
 import { type User } from '@domain/entities/user';
 
-export interface UserResponseDTO {
+export interface UserOutputDTO {
   id: string;
   email: string;
   name: string;
@@ -14,7 +14,7 @@ export interface UserResponseDTO {
 }
 // eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class UserMapper {
-  static toResponseDTO(user: User): UserResponseDTO {
+  static toResponseDTO(user: User): UserOutputDTO {
     return {
       id: user.id,
       email: user.email,
