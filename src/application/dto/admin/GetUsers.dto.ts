9a00f3c9ba605b26b280cda9e224fd60296ff0c3
aswin <nuzhumaki9@gmail.com>
2026-03-29 20:15:44 +0530

    style: improve code formatting and indentation in GetUsers.dto.ts

--- src/application/dto/admin/GetUsers.dto.ts
+++ src/application/dto/admin/GetUsers.dto.ts
@@ -2,28 +2,28 @@ import { Role } from "@domain/entities/user";
 import z from "zod";
 
 const optionalBoolean = z.preprocess(
-  (val) => {
-    if (val === undefined || val === null || val === "") return undefined;
-    return val;
-  },
-  z
-    .enum(["true", "false"])
-    .transform((val) => val === "true")
-    .optional(),
+	(val) => {
+		if (val === undefined || val === null || val === "") return undefined;
+		return val;
+	},
+	z
+		.enum(["true", "false"])
+		.transform((val) => val === "true")
+		.optional(),
 );
 export const getUsersSchema = z.object({
-  search: z.string().trim().optional().default(""),
-  page: z.coerce.number().min(1).catch(1),
-  limit: z.coerce.number().min(5).catch(5),
-  isBlocked: optionalBoolean,
-  isVerified: optionalBoolean,
-  role: z.enum(Role).optional(),
+	search: z.string().trim().optional().default(""),
+	page: z.coerce.number().min(1).catch(1),
+	limit: z.coerce.number().min(5).catch(5),
+	isBlocked: optionalBoolean,
+	isVerified: optionalBoolean,
+	role: z.enum(Role).optional(),
 });
 
 export type getUsersDTO = z.infer<typeof getUsersSchema>;
 
 // This is what the find all function returns for the user
 export interface PaginatedResult<T> {
-  data: T[];
-  totalItems: number;
+	data: T[];
+	totalItems: number;
 }
