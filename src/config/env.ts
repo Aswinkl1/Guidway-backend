55fece9f4532179febad8231c2540cc0fba49ff6
aswin <nuzhumaki9@gmail.com>
2026-04-09 19:35:07 +0530

    refactor(services):change all the urls to env

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,131 +1,156 @@
 import { z } from "zod";
 import "dotenv/config";
 const envSchema = z.object({
-	DATABASE_URL: z.url("DATABASE_URL must be a valid connection string"),
-	NODE_ENV: z
-		.enum(["development", "production", "test"])
-		.default("development"),
-	PORT: z.coerce.number().default(3000),
-	JWT_ACCESSTOKEN_SECRET: z
-		.string()
-		.min(1, "JWT_ACCESS_SECRET is required in .env"),
-	JWT_ACCESSTOKEN_EXPIRES_IN: z.string().default("15m"),
-	JWT_VERIFYTOKEN_SECRET: z
-		.string()
-		.min(1, "JWT_ACCESS_SECRET is required in .env"),
-	JWT_VERIFYTOKEN_EXPIRES_IN: z.string().default("15m"),
+  DATABASE_URL: z.url("DATABASE_URL must be a valid connection string"),
+  NODE_ENV: z
+    .enum(["development", "production", "test"])
+    .default("development"),
+  PORT: z.coerce.number().default(3000),
+  JWT_ACCESSTOKEN_SECRET: z
+    .string()
+    .min(1, "JWT_ACCESS_SECRET is required in .env"),
+  JWT_ACCESSTOKEN_EXPIRES_IN: z.string().default("15m"),
+  JWT_VERIFYTOKEN_SECRET: z
+    .string()
+    .min(1, "JWT_ACCESS_SECRET is required in .env"),
+  JWT_VERIFYTOKEN_EXPIRES_IN: z.string().default("15m"),
 
-	BASE_URL: z.url("BASE_URL must be a valid URL"),
-	CLIENT_BASE_URL: z.url("CLIENT_BASE_URL must be a valid URL"),
-	API_VERSION: z.string().default("/api/v1"),
-	NODEMAIL_EMAIL: z.email("NODEMAIL_EMAIL must be a valid email address"),
-	NODEMAIL_PASSWORD: z.string().min(1, "NODEMAIL_PASSWORD is required"),
-	AWS_S3_BUCKET_NAME: z.string().min(1, "S3_BUCKET_NAME is required in .env"),
-	AWS_ACCESS_KEY_ID: z.string().min(1, "AWS_ACCESS_KEY_ID is required in .env"),
-	AWS_SECRET_ACCESS_KEY: z
-		.string()
-		.min(1, "AWS_SECRET_ACCESS_KEY is required in .env"),
-	AWS_REGION: z.string().min(1, "AWS_REGION is required in .env"),
-	GOOGLE_CLIENT_ID: z.string().min(1, "GOOGLE_CLIENT_ID is required in .env"),
-	GOOGLE_CLIENT_SECRET: z
-		.string()
-		.min(1, "GOOGLE_CLIENT_SECRET is required in .env"),
-	LINKEDIN_CLIENT_ID: z
-		.string()
-		.min(1, "LINKEDIN_CLIENT_ID is required in .env"),
-	LINKEDIN_CLIENT_SECRET: z
-		.string()
-		.min(1, "LINKEDIN_CLIENT_SECRET is required in .env"),
-	COOKIE_MAX_AGE: z.coerce.number("Cookie max age is required "),
+  BASE_URL: z.url("BASE_URL must be a valid URL"),
+  CLIENT_BASE_URL: z.url("CLIENT_BASE_URL must be a valid URL"),
+  API_VERSION: z.string().default("/api/v1"),
+  NODEMAIL_EMAIL: z.email("NODEMAIL_EMAIL must be a valid email address"),
+  NODEMAIL_PASSWORD: z.string().min(1, "NODEMAIL_PASSWORD is required"),
+  AWS_S3_BUCKET_NAME: z.string().min(1, "S3_BUCKET_NAME is required in .env"),
+  AWS_ACCESS_KEY_ID: z.string().min(1, "AWS_ACCESS_KEY_ID is required in .env"),
+  AWS_SECRET_ACCESS_KEY: z
+    .string()
+    .min(1, "AWS_SECRET_ACCESS_KEY is required in .env"),
+  AWS_REGION: z.string().min(1, "AWS_REGION is required in .env"),
+  GOOGLE_CLIENT_ID: z.string().min(1, "GOOGLE_CLIENT_ID is required in .env"),
+  GOOGLE_CLIENT_SECRET: z
+    .string()
+    .min(1, "GOOGLE_CLIENT_SECRET is required in .env"),
+  LINKEDIN_CLIENT_ID: z
+    .string()
+    .min(1, "LINKEDIN_CLIENT_ID is required in .env"),
+  LINKEDIN_CLIENT_SECRET: z
+    .string()
+    .min(1, "LINKEDIN_CLIENT_SECRET is required in .env"),
+  COOKIE_MAX_AGE: z.coerce.number("Cookie max age is required "),
+  LINKEDIN_AUTHORIZATION_URL: z.url("Invalid LinkedIn authorization URL"),
+  LINKEDIN_CALLBACK_URL: z.url("Invalid LinkedIn callback URL"),
+  LINKEDIN_TOKEN_URL: z.url("Invalid LinkedIn token URL"),
+  LINKEDIN_GET_USERINFO_URL: z.url("Invalid LinkedIn user info URL"),
+
+  GOOGLE_CALLBACK_URL: z.url("Invalid LinkedIn user info URL"),
 });
 
 const parsedEnv = envSchema.parse(process.env);
 
 export class EnvConfig {
-	// --- Core Server ---
-	static get NODE_ENV(): string {
-		return parsedEnv.NODE_ENV;
-	}
+  // --- Core Server ---
+  static get NODE_ENV(): string {
+    return parsedEnv.NODE_ENV;
+  }
 
-	static get PORT(): number {
-		return parsedEnv.PORT;
-	}
+  static get PORT(): number {
+    return parsedEnv.PORT;
+  }
 
-	static get BASE_URL(): string {
-		return parsedEnv.BASE_URL;
-	}
+  static get BASE_URL(): string {
+    return parsedEnv.BASE_URL;
+  }
 
-	static get API_VERSION(): string {
-		return parsedEnv.API_VERSION;
-	}
+  static get API_VERSION(): string {
+    return parsedEnv.API_VERSION;
+  }
 
-	// --- Database ---
-	static get DATABASE_URL(): string {
-		return parsedEnv.DATABASE_URL;
-	}
+  // --- Database ---
+  static get DATABASE_URL(): string {
+    return parsedEnv.DATABASE_URL;
+  }
 
-	// --- Email ---
-	static get NODEMAIL_EMAIL(): string {
-		return parsedEnv.NODEMAIL_EMAIL;
-	}
+  // --- Email ---
+  static get NODEMAIL_EMAIL(): string {
+    return parsedEnv.NODEMAIL_EMAIL;
+  }
 
-	static get NODEMAIL_PASSWORD(): string {
-		return parsedEnv.NODEMAIL_PASSWORD;
-	}
+  static get NODEMAIL_PASSWORD(): string {
+    return parsedEnv.NODEMAIL_PASSWORD;
+  }
 
-	// --- JWT Auth ---
-	static get JWT_ACCESSTOKEN_SECRET(): string {
-		return parsedEnv.JWT_ACCESSTOKEN_SECRET;
-	}
+  // --- JWT Auth ---
+  static get JWT_ACCESSTOKEN_SECRET(): string {
+    return parsedEnv.JWT_ACCESSTOKEN_SECRET;
+  }
 
-	static get JWT_ACCESSTOKEN_EXPIRES_IN(): string {
-		return parsedEnv.JWT_ACCESSTOKEN_EXPIRES_IN;
-	}
-	static get JWT_VERIFYTOKEN_SECRET(): string {
-		return parsedEnv.JWT_VERIFYTOKEN_SECRET;
-	}
+  static get JWT_ACCESSTOKEN_EXPIRES_IN(): string {
+    return parsedEnv.JWT_ACCESSTOKEN_EXPIRES_IN;
+  }
+  static get JWT_VERIFYTOKEN_SECRET(): string {
+    return parsedEnv.JWT_VERIFYTOKEN_SECRET;
+  }
 
-	static get JWT_VERIFYTOKEN_EXPIRES_IN(): string {
-		return parsedEnv.JWT_VERIFYTOKEN_EXPIRES_IN;
-	}
+  static get JWT_VERIFYTOKEN_EXPIRES_IN(): string {
+    return parsedEnv.JWT_VERIFYTOKEN_EXPIRES_IN;
+  }
 
-	static get CLIENT_BASE_URL(): string {
-		return parsedEnv.CLIENT_BASE_URL;
-	}
+  static get CLIENT_BASE_URL(): string {
+    return parsedEnv.CLIENT_BASE_URL;
+  }
 
-	static get S3_BUCKET_NAME(): string {
-		return parsedEnv.AWS_S3_BUCKET_NAME;
-	}
+  static get S3_BUCKET_NAME(): string {
+    return parsedEnv.AWS_S3_BUCKET_NAME;
+  }
 
-	static get AWS_ACCESS_KEY_ID(): string {
-		return parsedEnv.AWS_ACCESS_KEY_ID;
-	}
+  static get AWS_ACCESS_KEY_ID(): string {
+    return parsedEnv.AWS_ACCESS_KEY_ID;
+  }
 
-	static get AWS_SECRET_ACCESS_KEY(): string {
-		return parsedEnv.AWS_SECRET_ACCESS_KEY;
-	}
+  static get AWS_SECRET_ACCESS_KEY(): string {
+    return parsedEnv.AWS_SECRET_ACCESS_KEY;
+  }
 
-	static get AWS_REGION(): string {
-		return parsedEnv.AWS_REGION;
-	}
+  static get AWS_REGION(): string {
+    return parsedEnv.AWS_REGION;
+  }
 
-	static get GOOGLE_CLIENT_ID(): string {
-		return parsedEnv.GOOGLE_CLIENT_ID;
-	}
+  static get GOOGLE_CLIENT_ID(): string {
+    return parsedEnv.GOOGLE_CLIENT_ID;
+  }
 
-	static get GOOGLE_CLIENT_SECRET(): string {
-		return parsedEnv.GOOGLE_CLIENT_SECRET;
-	}
+  static get GOOGLE_CLIENT_SECRET(): string {
+    return parsedEnv.GOOGLE_CLIENT_SECRET;
+  }
 
-	static get LINKEDIN_CLIENT_ID(): string {
-		return parsedEnv.LINKEDIN_CLIENT_ID;
-	}
+  static get LINKEDIN_CLIENT_ID(): string {
+    return parsedEnv.LINKEDIN_CLIENT_ID;
+  }
 
-	static get LINKEDIN_CLIENT_SECRET(): string {
-		return parsedEnv.LINKEDIN_CLIENT_SECRET;
-	}
+  static get LINKEDIN_CLIENT_SECRET(): string {
+    return parsedEnv.LINKEDIN_CLIENT_SECRET;
+  }
 
-	static get COOKIE_MAX_AGE(): number {
-		return parsedEnv.COOKIE_MAX_AGE;
-	}
+  static get COOKIE_MAX_AGE(): number {
+    return parsedEnv.COOKIE_MAX_AGE;
+  }
+
+  static get LINKEDIN_AUTHORIZATION_URL(): string {
+    return parsedEnv.LINKEDIN_AUTHORIZATION_URL;
+  }
+
+  static get LINKEDIN_CALLBACK_URL(): string {
+    return parsedEnv.LINKEDIN_CALLBACK_URL;
+  }
+
+  static get LINKEDIN_TOKEN_URL(): string {
+    return parsedEnv.LINKEDIN_TOKEN_URL;
+  }
+
+  static get LINKEDIN_GET_USERINFO_URL(): string {
+    return parsedEnv.LINKEDIN_GET_USERINFO_URL;
+  }
+  static get GOOGLE_CALLBACK_URL(): string {
+    return parsedEnv.GOOGLE_CALLBACK_URL;
+  }
 }
--- src/infrastructure/services/PassportService.ts
+++ src/infrastructure/services/PassportService.ts
@@ -10,83 +10,83 @@ import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import { Strategy as OAuth2Strategy } from "passport-oauth2";
 @injectable()
 export class PassportConfig {
-	constructor(
-		@inject(TYPES.OAuthUseCase) private readonly _oAuthUseCase: IOAuthUseCase,
-	) {}
+  constructor(
+    @inject(TYPES.OAuthUseCase) private readonly _oAuthUseCase: IOAuthUseCase,
+  ) {}
 
-	public config(): void {
-		passport.use(
-			new GoogleStrategy(
-				{
-					clientID: EnvConfig.GOOGLE_CLIENT_ID,
-					clientSecret: EnvConfig.GOOGLE_CLIENT_SECRET,
-					callbackURL: "http://localhost:3000/api/v1/auth/google/callback",
-					scope: ["openid", "profile", "email"],
-				},
-				async (_, __, profile, done) => {
-					try {
-						console.log(profile);
-						const email = profile.emails?.[0]?.value;
-						const name = profile.displayName;
-						const providerId = profile.id;
+  public config(): void {
+    passport.use(
+      new GoogleStrategy(
+        {
+          clientID: EnvConfig.GOOGLE_CLIENT_ID,
+          clientSecret: EnvConfig.GOOGLE_CLIENT_SECRET,
+          callbackURL: EnvConfig.GOOGLE_CALLBACK_URL,
+          scope: ["openid", "profile", "email"],
+        },
+        async (_, __, profile, done) => {
+          try {
+            console.log(profile);
+            const email = profile.emails?.[0]?.value;
+            const name = profile.displayName;
+            const providerId = profile.id;
 
-						if (!email) {
-							return done(new Error("No email found in Google profile"));
-						}
+            if (!email) {
+              return done(new Error("No email found in Google profile"));
+            }
 
-						const oAuthInputDTO = {
-							email,
-							name,
-							providerId,
-							role: Role.MENTEE,
-						};
+            const oAuthInputDTO = {
+              email,
+              name,
+              providerId,
+              role: Role.MENTEE,
+            };
 
-						const result = await this._oAuthUseCase.execute(oAuthInputDTO);
-						done(null, result as unknown as Express.User);
-					} catch (error) {
-						done(error as Error);
-					}
-				},
-			),
-		);
+            const result = await this._oAuthUseCase.execute(oAuthInputDTO);
+            done(null, result as unknown as Express.User);
+          } catch (error) {
+            done(error as Error);
+          }
+        },
+      ),
+    );
 
-		passport.use(
-			new OAuth2Strategy(
-				{
-					authorizationURL: "https://www.linkedin.com/oauth/v2/authorization",
-					tokenURL: "https://www.linkedin.com/oauth/v2/accessToken",
-					clientID: EnvConfig.LINKEDIN_CLIENT_ID,
-					clientSecret: EnvConfig.LINKEDIN_CLIENT_SECRET,
-					callbackURL: "http://localhost:3000/api/v1/auth/linkedin/callback",
-					scope: ["openid", "profile", "email"],
-				},
-				// biome-ignore lint/suspicious/noExplicitAny: third-party API returns an unpredictable object
-				async (accessToken: string, _: any, __: any, done: any) => {
-					try {
-						console.log("yododod");
-						const response = await axios.get(
-							"https://api.linkedin.com/v2/userinfo",
-							{
-								headers: { Authorization: `Bearer ${accessToken}` },
-								httpsAgent: new https.Agent({ family: 4 }),
-							},
-						);
-						const userData = response.data;
+    passport.use(
+      new OAuth2Strategy(
+        {
+          authorizationURL: EnvConfig.LINKEDIN_AUTHORIZATION_URL,
+          tokenURL: EnvConfig.LINKEDIN_TOKEN_URL,
+          clientID: EnvConfig.LINKEDIN_CLIENT_ID,
+          clientSecret: EnvConfig.LINKEDIN_CLIENT_SECRET,
+          callbackURL: EnvConfig.LINKEDIN_CALLBACK_URL,
+          scope: ["openid", "profile", "email"],
+        },
+        // biome-ignore lint/suspicious/noExplicitAny: third-party API returns an unpredictable object
+        async (accessToken: string, _: any, __: any, done: any) => {
+          try {
+            console.log("yododod");
+            const response = await axios.get(
+              EnvConfig.LINKEDIN_GET_USERINFO_URL,
+              {
+                headers: { Authorization: `Bearer ${accessToken}` },
+                httpsAgent: new https.Agent({ family: 4 }),
+              },
+            );
+            const userData = response.data;
 
-						const oAuthInputDTO = {
-							email: userData.email,
-							name: userData.name,
-							providerId: userData.sub,
-							role: Role.MENTOR,
-						};
-						const result = await this._oAuthUseCase.execute(oAuthInputDTO);
-						done(null, result as unknown as Express.User);
-						console.log(userData);
-					} catch (error) {
-						done(error, null);
-					}
-				},
-			),
-		);
-	}
+            const oAuthInputDTO = {
+              email: userData.email,
+              name: userData.name,
+              providerId: userData.sub,
+              role: Role.MENTOR,
+            };
+
+            const result = await this._oAuthUseCase.execute(oAuthInputDTO);
+            done(null, result as unknown as Express.User);
+          } catch (error) {
+            done(error, null);
+          }
+        },
+      ),
+    );
+  }
 }
