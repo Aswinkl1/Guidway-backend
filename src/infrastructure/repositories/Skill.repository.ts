e6111bad39dbccc29efcc519810cbb1482db9f60
aswin <nuzhumaki9@gmail.com>
2026-05-05 22:38:37 +0530

    feat: implement findAll method in SkillRepository for skills retrieval

--- src/infrastructure/repositories/Skill.repository.ts
+++ src/infrastructure/repositories/Skill.repository.ts
@@ -1,6 +1,13 @@
+import type { GetSkillsQueryDto } from "@application/dto/mentor/skill.dto";
 import type { ISkillRepository } from "@application/ports/repository/ISkill.repositorty";
+import { TYPES } from "@config/DI-container/TYPES";
 import { Skill } from "@domain/mentor/entities/skills.entity";
-import type { Prisma, Skill as PrismaSkill } from "generated/prisma/client";
+import type {
+	Prisma,
+	PrismaClient,
+	Skill as PrismaSkill,
+} from "generated/prisma/client";
+import { inject } from "inversify";
 import { BaseRepository } from "./BaseRepository";
 export class SkillRepository
 	extends BaseRepository<
@@ -11,6 +18,19 @@ export class SkillRepository
 	>
 	implements ISkillRepository
 {
+	constructor(@inject(TYPES.PrismaClient) private _prisma: PrismaClient) {
+		super(_prisma.skill);
+	}
+	async findAll(filter: GetSkillsQueryDto): Promise<Skill[] | []> {
+		const { search } = filter;
+
+		const record = await this._prisma.skill.findMany({
+			where: { name: search },
+		});
+
+		return record.map((r) => this.toDomain(r));
+	}
+
 	protected toDomain(record: PrismaSkill): Skill {
 		return Skill.create(record);
 	}
