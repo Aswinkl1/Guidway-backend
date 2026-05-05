3bf83db62bd020b8e7687d4bd42658c8142fe4e4
aswin <nuzhumaki9@gmail.com>
2026-05-05 23:05:07 +0530

    feat: implement GetSkill use case for skills retrieval

--- src/application/ports/usecase/mentor/skills/IGetSkills.usecase.ts
+++ src/application/ports/usecase/mentor/skills/IGetSkills.usecase.ts
@@ -2,5 +2,5 @@ import type { GetSkillsQueryDto } from "@application/dto/mentor/skill.dto";
 import type { SkillOutputDto } from "@application/mappers/skills.mapper";
 
 export interface IGetSkillsUsecase {
-	execute(dto: GetSkillsQueryDto): Promise<SkillOutputDto>;
+	execute(dto: GetSkillsQueryDto): Promise<SkillOutputDto[]>;
 }
---
+++ src/application/useCases/mentor/skill/GetSkill.usecase.ts
@@ -0,0 +1,16 @@
+import type { GetSkillsQueryDto } from "@application/dto/mentor/skill.dto";
+import {
+	SkillMapper,
+	type SkillOutputDto,
+} from "@application/mappers/skills.mapper";
+import type { ISkillRepository } from "@application/ports/repository/ISkill.repositorty";
+import type { IGetSkillsUsecase } from "@application/ports/usecase/mentor/skills/IGetSkills.usecase";
+
+export class GetSkillUsecase implements IGetSkillsUsecase {
+	constructor(private readonly _skillReporitory: ISkillRepository) {}
+	async execute(dto: GetSkillsQueryDto): Promise<SkillOutputDto[]> {
+		const record = await this._skillReporitory.findAll(dto);
+
+		return SkillMapper.toOutputList(record);
+	}
+}
