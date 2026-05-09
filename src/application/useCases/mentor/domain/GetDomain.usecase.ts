37c7bb7339e30097a123777c221713915641c65c
aswin <nuzhumaki9@gmail.com>
2026-05-10 00:14:45 +0530

    refactor: enhance GetDomainUsecase with dependency injection for DomainRepository

--- src/application/useCases/mentor/domain/GetDomain.usecase.ts
+++ src/application/useCases/mentor/domain/GetDomain.usecase.ts
@@ -5,9 +5,15 @@ import {
 } from "@application/mappers/domain.mapper";
 import type { IDomainRepository } from "@application/ports/repository/IDomain.repository";
 import type { IGetDomainUsecase } from "@application/ports/usecase/mentor/Domian/IGetDomain.usecase";
+import { TYPES } from "@config/DI-container/TYPES";
+import { inject, injectable } from "inversify";
 
+@injectable()
 export class GetDomainUsecase implements IGetDomainUsecase {
-	constructor(private readonly _domainRepository: IDomainRepository) {}
+	constructor(
+		@inject(TYPES.DomainRepository)
+		private readonly _domainRepository: IDomainRepository,
+	) {}
 
 	async execute(filter: GetDomainQueryDto): Promise<IDomainOutputDTO[]> {
 		const record = await this._domainRepository.findAll(filter);
