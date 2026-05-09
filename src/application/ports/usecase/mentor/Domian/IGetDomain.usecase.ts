18d68f1766955a344fd7c42298db51971aab9cde
aswin <nuzhumaki9@gmail.com>
2026-05-10 00:02:21 +0530

    feat: implement GetDomainUsecase for domain retrieval

--- src/application/ports/usecase/mentor/Domian/IGetDomain.usecase.ts
+++ src/application/ports/usecase/mentor/Domian/IGetDomain.usecase.ts
@@ -2,5 +2,5 @@ import type { GetDomainQueryDto } from "@application/dto/mentor/domain.dto";
 import type { IDomainOutputDTO } from "@application/mappers/domain.mapper";
 
 export interface IGetDomainUsecase {
-	execute(dto: GetDomainQueryDto): Promise<IDomainOutputDTO>;
+	execute(filter: GetDomainQueryDto): Promise<IDomainOutputDTO[]>;
 }
---
+++ src/application/useCases/mentor/domain/GetDomain.usecase.ts
@@ -0,0 +1,17 @@
+import type { GetDomainQueryDto } from "@application/dto/mentor/domain.dto";
+import {
+	DomainMapper,
+	type IDomainOutputDTO,
+} from "@application/mappers/domain.mapper";
+import type { IDomainRepository } from "@application/ports/repository/IDomain.repository";
+import type { IGetDomainUsecase } from "@application/ports/usecase/mentor/Domian/IGetDomain.usecase";
+
+export class GetDomainUsecase implements IGetDomainUsecase {
+	constructor(private readonly _domainRepository: IDomainRepository) {}
+
+	async execute(filter: GetDomainQueryDto): Promise<IDomainOutputDTO[]> {
+		const record = await this._domainRepository.findAll(filter);
+
+		return record.map((d) => DomainMapper.toOutput(d));
+	}
+}
