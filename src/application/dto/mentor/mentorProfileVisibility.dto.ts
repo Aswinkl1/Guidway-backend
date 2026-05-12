9e11d0a9a048b29d24594a7bc625741c71b8e803
aswin <nuzhumaki9@gmail.com>
2026-05-13 00:19:41 +0530

    Remove mentor profile visibility DTO and validation schema

--- src/application/dto/mentor/mentorProfileVisibility.dto.ts
+++
@@ -1,9 +0,0 @@
-import z from "zod";
-
-export const updateMentorVisibilitySchema = z.object({
-	isVisbile: z.boolean(),
-});
-
-export type updateMentorVisibilityDTO = z.infer<
-	typeof updateMentorVisibilitySchema
->;
