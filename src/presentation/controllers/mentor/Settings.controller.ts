6585f40dbc53e6e2f221e5d1b6be594f79ad8bef
aswin <nuzhumaki9@gmail.com>
2026-05-13 00:20:32 +0530

    Add updateMentorStatus method to SettingController and update ISettingsController interface

--- src/presentation/controllers/mentor/Settings.controller.ts
+++ src/presentation/controllers/mentor/Settings.controller.ts
@@ -1,4 +1,6 @@
+import type { MentorStatusUpdateDTO } from "@application/dto/mentor/MentorStatusUpdate.dto";
 import type { UpdateBookingRulesDTO } from "@application/dto/mentor/mentorBookingRules.dto";
+import type { IMentorStatusUpdateUsecase } from "@application/ports/usecase/mentor/IMentorStatusUpdate.usecase";
 import type { IUpdateMentorBookingRulesUsecase } from "@application/ports/usecase/mentor/IUpdateMentorBookingRules.usecase";
 import { TYPES } from "@config/DI-container/TYPES";
 import { NotFoundError } from "@domain/errors/UserError";
@@ -13,6 +15,8 @@ export class SettingController implements ISettingsController {
 	constructor(
 		@inject(TYPES.UpdateMentorBookingRulesUsecase)
 		private readonly _mentorBookingRulesUsecase: IUpdateMentorBookingRulesUsecase,
+		@inject(TYPES.MentorStatusUpdateUsecase)
+		private readonly _mentorStatusUpdateUsecase: IMentorStatusUpdateUsecase,
 	) {}
 
 	updateMentorBookingRules = async (
@@ -28,5 +32,18 @@ export class SettingController implements ISettingsController {
 		await this._mentorBookingRulesUsecase.execute(mentorId, parsed);
 
 		res.status(HTTPSTATUS.OK).json(createSuccess("siccess", {}));
+	};
+
+	updateMentorStatus = async (req: Request, res: Response): Promise<void> => {
+		const parsed = req.validated?.body as MentorStatusUpdateDTO;
+		const mentorId = req.user?.userId;
+
+		if (!mentorId) {
+			throw new NotFoundError("mentor id not found");
+		}
+
+		await this._mentorStatusUpdateUsecase.execute(mentorId, parsed);
+
+		res.status(HTTPSTATUS.OK).json(createSuccess("success", {}));
 	};
 }
