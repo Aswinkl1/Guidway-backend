027704fd9d6e125ce31f5660a18afc3468340e15
aswin <nuzhumaki9@gmail.com>
2026-05-13 22:00:56 +0530

    Implement deleteSession method in SessionController and update ISessionController interface

--- src/presentation/interface/controllers/ISession.controller.ts
+++ src/presentation/interface/controllers/ISession.controller.ts
@@ -3,4 +3,5 @@ import type { Request, Response } from "express";
 export interface ISessionController {
 	addSession(req: Request, res: Response): Promise<void>;
 	editSession(req: Request, res: Response): Promise<void>;
+	deleteSession(req: Request, res: Response): Promise<void>;
 }
