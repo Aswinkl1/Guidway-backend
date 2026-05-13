38281f6539133402838ded9caf7d4917816c4665
aswin <nuzhumaki9@gmail.com>
2026-05-13 17:31:36 +0530

    Add SessionRepository class for session management

---
+++ src/infrastructure/repositories/Session.repository.ts
@@ -0,0 +1,31 @@
+import type { ISessionRepository } from "@application/ports/repository/ISession.respository";
+import { Session } from "@domain/session/session.entitiy";
+import type { Prisma, Session as PrismaSession } from "generated/prisma/client";
+import { BaseRepository } from "./BaseRepository";
+
+export class SessionRepository
+	extends BaseRepository<
+		PrismaSession,
+		Session,
+		Prisma.SessionCreateInput,
+		Prisma.SessionUpdateInput
+	>
+	implements ISessionRepository
+{
+	protected toDomain(record: PrismaSession): Session {
+		return Session.Create(record);
+	}
+	protected toPersistence(
+		entity: Session,
+	): Omit<Prisma.SessionCreateInput, "createdAt" | "updatedAt"> {
+		return {
+			id: entity.id,
+			description: entity.description,
+			duration: entity.duration,
+			mentor: { connect: { userId: entity.mentorId } },
+			name: entity.name,
+			deletedAt: entity.deletedAt,
+			isActive: entity.isActive,
+		};
+	}
+}
