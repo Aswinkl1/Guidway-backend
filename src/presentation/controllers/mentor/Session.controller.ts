c2a23ee05bd3a985fd79513ceb1667202afeeff5
aswin <nuzhumaki9@gmail.com>
2026-05-13 21:08:03 +0530

    Add editSession method to SessionController and update ISessionController interface

--- src/presentation/controllers/mentor/Session.controller.ts
+++ src/presentation/controllers/mentor/Session.controller.ts
@@ -1,5 +1,9 @@
-import type { CreateSessionDTO } from "@application/dto/mentor/session.dto";
+import type {
+	CreateSessionDTO,
+	editSessionDTO,
+} from "@application/dto/mentor/session.dto";
 import type { IAddSessionUsecase } from "@application/ports/usecase/mentor/session/IAddSession.usecase";
+import type { IEditSessionUsecase } from "@application/ports/usecase/mentor/session/IEditSession.usecase";
 import { TYPES } from "@config/DI-container/TYPES";
 import { NotFoundError } from "@domain/errors/UserError";
 import HTTPSTATUS from "@presentation/constants/httpStatus";
@@ -12,6 +16,8 @@ export class SessionController implements ISessionController {
 	constructor(
 		@inject(TYPES.AddSessionUsecase)
 		private readonly _addSessionUsecase: IAddSessionUsecase,
+		@inject(TYPES.EditSessionUsecase)
+		private readonly _editSessionUsecase: IEditSessionUsecase,
 	) {}
 
 	addSession = async (req: Request, res: Response): Promise<void> => {
@@ -24,5 +30,16 @@ export class SessionController implements ISessionController {
 		const record = await this._addSessionUsecase.execute(mentorId, parsed);
 
 		res.status(HTTPSTATUS.CREATED).json(createSuccess("success", record));
+	};
+
+	editSession = async (req: Request, res: Response): Promise<void> => {
+		const parsed = req.validated?.body as editSessionDTO;
+		const mentorId = req.user?.userId;
+		if (!mentorId) {
+			throw new NotFoundError("mentor id not found");
+		}
+
+		const record = await this._editSessionUsecase.execute(mentorId, parsed);
+		res.status(HTTPSTATUS.OK).json(createSuccess("success", record));
 	};
 }
--- src/presentation/interface/controllers/ISession.controller.ts
+++ src/presentation/interface/controllers/ISession.controller.ts
@@ -2,4 +2,5 @@ import type { Request, Response } from "express";
 
 export interface ISessionController {
 	addSession(req: Request, res: Response): Promise<void>;
+	editSession(req: Request, res: Response): Promise<void>;
 }
