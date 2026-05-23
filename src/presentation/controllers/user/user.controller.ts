e55384f376f9e23e41cd38ea8c2f4bd054355954
aswin <nuzhumaki9@gmail.com>
2026-05-23 20:56:52 +0530

    Refactor UserController to improve dependency injection and formatting

--- src/presentation/controllers/user/user.controller.ts
+++ src/presentation/controllers/user/user.controller.ts
@@ -1,12 +1,17 @@
 import type { publicListMentorDto } from "@application/dto/mentor/listMentor.dto";
 import type { IListMentorsUsecase } from "@application/ports/usecase/IListMentor.usecase";
+import { TYPES } from "@config/DI-container/TYPES";
 import HTTPSTATUS from "@presentation/constants/httpStatus";
 import { createSuccess } from "@presentation/helper/response.util";
 import type { IUserController } from "@presentation/interface/controllers/user/IUser.controller";
 import type { Request, Response } from "express";
-
+import { inject, injectable } from "inversify";
+@injectable()
 export class UserController implements IUserController {
-	constructor(private readonly _listMentorsUsecase: IListMentorsUsecase) {}
+	constructor(
+		@inject(TYPES.ListMentorUsecase)
+		private readonly _listMentorsUsecase: IListMentorsUsecase,
+	) {}
 
 	getListMentors = async (req: Request, res: Response): Promise<void> => {
 		const parsed = req.validated?.query as publicListMentorDto;
