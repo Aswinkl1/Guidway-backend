60fa029e75703f0ac92351048b589ca92919daaf
aswin <nuzhumaki9@gmail.com>
2026-05-23 19:47:49 +0530

    Add UserController and IUserController interface for mentor retrieval

---
+++ src/presentation/controllers/user/user.controller.ts
@@ -0,0 +1,18 @@
+import type { publicListMentorDto } from "@application/dto/mentor/listMentor.dto";
+import type { IListMentorsUsecase } from "@application/ports/usecase/IListMentor.usecase";
+import HTTPSTATUS from "@presentation/constants/httpStatus";
+import { createSuccess } from "@presentation/helper/response.util";
+import type { IUserController } from "@presentation/interface/controllers/user/IUser.controller";
+import type { Request, Response } from "express";
+
+export class UserController implements IUserController {
+	constructor(private readonly _listMentorsUsecase: IListMentorsUsecase) {}
+
+	getListMentors = async (req: Request, res: Response): Promise<void> => {
+		const parsed = req.validated?.query as publicListMentorDto;
+
+		const data = await this._listMentorsUsecase.execute(parsed);
+
+		res.status(HTTPSTATUS.OK).json(createSuccess("success ", data));
+	};
+}
