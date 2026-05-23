2ed1a6607967e9444823105c90d3eba733528098
aswin <nuzhumaki9@gmail.com>
2026-05-23 19:21:37 +0530

    Refactor list mentor DTO to use BaseListMentorSchema with optional fields for improved flexibility

--- src/application/dto/mentor/listMentor.dto.ts
+++ src/application/dto/mentor/listMentor.dto.ts
@@ -1,9 +1,22 @@
+import { MentorStatus } from "@domain/mentor/mentor.entity";
 import z from "zod";
 
-export const listMentorSchema = z.object({
-	search: z.string().trim(),
-	cursor: z.uuid(),
-	limit: z.number().default(5),
+export const BaseListMentorSchema = z.object({
+	search: z.string().trim().optional(),
+	cursor: z.uuid().optional(),
+	limit: z.number().optional().default(5),
+	domainId: z.uuid().optional(),
+	status: z.enum(MentorStatus).optional(),
+	isVerified: z.boolean().optional(),
 });
 
-export type listMentorDto = z.infer<typeof listMentorSchema>;
+export type listMentorDto = z.infer<typeof BaseListMentorSchema>;
+
+export const publicListMentorSchema = BaseListMentorSchema.pick({
+	cursor: true,
+	domainId: true,
+	limit: true,
+	search: true,
+});
+
+export type publicListMentorDto = z.infer<typeof publicListMentorSchema>;
