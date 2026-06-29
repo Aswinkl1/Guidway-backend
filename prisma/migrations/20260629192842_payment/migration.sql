bee828355f0bbacc9ec925475650d241bbb6f4e6
aswin <nuzhumaki9@gmail.com>
2026-06-30 02:36:47 +0530

    feat(database): create initial payment and booking tables with foreign key constraints

--- prisma/migrations/20260629192842_payment/migration.sql
+++ prisma/migrations/20260629192842_payment/migration.sql
@@ -4,13 +4,12 @@ CREATE TYPE "BookingStatus" AS ENUM ('CONFIRMED', 'CANCELLED', 'COMPLETED');
 -- CreateEnum
 CREATE TYPE "PaymentStatus" AS ENUM ('PENDING', 'SUCCESS', 'FAILED');
 
--- AlterTable
-ALTER TABLE "slots" ALTER COLUMN "date" SET DATA TYPE DATE;
-
 -- CreateTable
 CREATE TABLE "bookings" (
     "id" TEXT NOT NULL,
     "slotId" TEXT NOT NULL,
+    "mentorId" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
     "sessionId" TEXT NOT NULL,
     "sessionTitle" TEXT NOT NULL,
     "startTime" TIMESTAMP(3) NOT NULL,
@@ -29,12 +28,11 @@ CREATE TABLE "bookings" (
 -- CreateTable
 CREATE TABLE "payments" (
     "id" TEXT NOT NULL,
-    "bookingId" TEXT NOT NULL,
     "paymentProvider" TEXT NOT NULL,
     "paymentId" TEXT NOT NULL,
     "paymentOrderId" TEXT NOT NULL,
     "paymentSignature" TEXT NOT NULL,
-    "amount" INTEGER NOT NULL,
+    "amountInMinorUnit" INTEGER NOT NULL,
     "currency" "currency" NOT NULL,
     "status" "PaymentStatus" NOT NULL DEFAULT 'PENDING',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -50,4 +48,4 @@ ALTER TABLE "bookings" ADD CONSTRAINT "bookings_slotId_fkey" FOREIGN KEY ("slotI
 ALTER TABLE "bookings" ADD CONSTRAINT "bookings_sessionId_fkey" FOREIGN KEY ("sessionId") REFERENCES "Session"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "payments" ADD CONSTRAINT "payments_bookingId_fkey" FOREIGN KEY ("bookingId") REFERENCES "bookings"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+ALTER TABLE "bookings" ADD CONSTRAINT "bookings_paymentId_fkey" FOREIGN KEY ("paymentId") REFERENCES "payments"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
