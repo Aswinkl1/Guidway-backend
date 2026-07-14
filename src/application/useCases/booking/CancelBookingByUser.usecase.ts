c2485027576996d9e1b29cb7826f12c639be34a7
aswin <nuzhumaki9@gmail.com>
2026-07-15 01:08:17 +0530

    feat(booking): Implement booking cancellation logic with error handling

--- src/application/useCases/booking/CancelBookingByUser.usecase.ts
+++ src/application/useCases/booking/CancelBookingByUser.usecase.ts
@@ -1,6 +1,8 @@
+import { ForbiddenError } from "@application/errors/ForbidenError";
 import type { IBookingRepository } from "@application/ports/repository/IBooking.repository";
 import type { ICancelBookingByUserUsecase } from "@application/ports/usecase/booking/ICancelBookingByUser.usecase";
 import { TYPES } from "@config/DI-container/TYPES";
+import { NotFoundError } from "@domain/errors/UserError";
 import { inject, injectable } from "inversify";
 
 @injectable()
@@ -10,6 +12,15 @@ export class CancelBookingByUserUsecase implements ICancelBookingByUserUsecase {
 		private readonly _bookingRepository: IBookingRepository,
 	) {}
 	async execute(userId: string, bookingId: string): Promise<void> {
+		const booking = await this._bookingRepository.findById(bookingId);
+
+		if (!booking) {
+			throw new NotFoundError("Booking not found");
+		}
+
+		if (booking.userId !== userId) {
+			throw new ForbiddenError("You are not authorized to cancel this booking");
+		}
 		await this._bookingRepository.cancelBooking(userId, bookingId);
 	}
 }
