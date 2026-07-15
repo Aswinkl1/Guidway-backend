01b56e4e367cd8acd36e6fa56bab6bcb792b769d
aswin <nuzhumaki9@gmail.com>
2026-07-15 10:09:57 +0530

    refactor(booking): Improve CancelBookingByMentor use case structure and dependency injection

--- src/application/useCases/booking/CancelBookingByMentor.usecase.ts
+++ src/application/useCases/booking/CancelBookingByMentor.usecase.ts
@@ -1,15 +1,20 @@
 import { ForbiddenError } from "@application/errors/ForbidenError";
 import type { IBookingRepository } from "@application/ports/repository/IBooking.repository";
 import type { ICancelBookingByMentorUsecase } from "@application/ports/usecase/booking/ICancelBookingByMentor.usecase";
+import { TYPES } from "@config/DI-container/TYPES";
 import { ConflictError } from "@domain/errors/ConflictError";
 import { NotFoundError } from "@domain/errors/UserError";
-
+import { inject, injectable } from "inversify";
+@injectable()
 export class CancelBookingByMentorUsecase
 	implements ICancelBookingByMentorUsecase
 {
-	constructor(private readonly bookingRepository: IBookingRepository) {}
+	constructor(
+		@inject(TYPES.BookingRepository)
+		private readonly _bookingRepository: IBookingRepository,
+	) {}
 	async execute(mentorId: string, bookingId: string): Promise<void> {
-		const booking = await this.bookingRepository.findById(bookingId);
+		const booking = await this._bookingRepository.findById(bookingId);
 		if (!booking) {
 			throw new NotFoundError("booking not found");
 		}
@@ -27,6 +32,6 @@ export class CancelBookingByMentorUsecase
 			);
 		}
 
-		return this.bookingRepository.cancelBooking(mentorId, bookingId);
+		return this._bookingRepository.cancelBooking(mentorId, bookingId);
 	}
 }
