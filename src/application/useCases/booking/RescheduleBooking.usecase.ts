8ca8fe783f5bd632d77219a89f23ffe40408dac0
aswin <nuzhumaki9@gmail.com>
2026-07-16 00:51:32 +0530

    feat(booking): Integrate RescheduleBookingUsecase into DI container and update related bindings

--- src/application/useCases/booking/RescheduleBooking.usecase.ts
+++ src/application/useCases/booking/RescheduleBooking.usecase.ts
@@ -2,10 +2,15 @@ import type { rescheduleBookingDto } from "@application/dto/booking/rescheduleBo
 import { ForbiddenError } from "@application/errors/ForbidenError";
 import type { IBookingRepository } from "@application/ports/repository/IBooking.repository";
 import type { IRescheduleBookingUsecase } from "@application/ports/usecase/booking/IRescheduleBooking.usecase";
+import { TYPES } from "@config/DI-container/TYPES";
 import { NotFoundError } from "@domain/errors/UserError";
-
+import { inject, injectable } from "inversify";
+@injectable()
 export class RescheduleBookingUsecase implements IRescheduleBookingUsecase {
-	constructor(private readonly bookingRepository: IBookingRepository) {}
+	constructor(
+		@inject(TYPES.BookingRepository)
+		private readonly bookingRepository: IBookingRepository,
+	) {}
 	async execute(
 		userId: string,
 		data: rescheduleBookingDto,
