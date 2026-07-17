20e0342015f245802fd8f29e962db359f4088d31
aswin <nuzhumaki9@gmail.com>
2026-07-17 19:41:47 +0530

    feat(review): Add DeleteReview use case and update ReviewController

--- src/presentation/controllers/booking/review.controller.ts
+++ src/presentation/controllers/booking/review.controller.ts
@@ -1,5 +1,6 @@
 import type { addReviewDto } from "@application/dto/booking/review.dto";
 import type { IAddReviewUsecase } from "@application/ports/usecase/booking/Review/IAddReview.usecase";
+import type { IDeleteReviewUsecase } from "@application/ports/usecase/booking/Review/IDeleteReview.usecase";
 import { TYPES } from "@config/DI-container/TYPES";
 import { NotFoundError } from "@domain/errors/UserError";
 import HTTPSTATUS from "@presentation/constants/httpStatus";
@@ -12,6 +13,8 @@ export class ReviewController implements IReviewController {
 	constructor(
 		@inject(TYPES.AddReviewUsecase)
 		private readonly _addReviewUsecase: IAddReviewUsecase,
+		@inject(TYPES.DeleteReviewUsecase)
+		private readonly _deleteReviewUsecase: IDeleteReviewUsecase,
 	) {}
 
 	addReview = async (req: Request, res: Response): Promise<void> => {
@@ -29,5 +32,20 @@ export class ReviewController implements IReviewController {
 			.json(createSuccess("succesfully created review", {}));
 	};
 
-	deleteReview = async (req: Request, res: Response): Promise<void> => {};
+	deleteReview = async (req: Request, res: Response): Promise<void> => {
+		const userId = req.user?.userId;
+		const reviewId = req.params.id;
+
+		if (!userId) {
+			throw new NotFoundError("user not found");
+		}
+		if (!reviewId || typeof reviewId !== "string") {
+			throw new NotFoundError("user not found");
+		}
+		await this._deleteReviewUsecase.execute(userId, reviewId.trim());
+
+		res
+			.status(HTTPSTATUS.NO_CONTENT)
+			.json(createSuccess("succesfully created review", {}));
+	};
 }
