feba069bcb36a3120619fb0913b2b8e2e3ad9f46
aswin <nuzhumaki9@gmail.com>
2026-07-29 00:59:12 +0530

    feat(video-call): implement IVedioCall use case and add socket.io dependency

---
+++ src/application/useCases/booking/VedioCall.usecase.ts
@@ -0,0 +1,25 @@
+import { ForbiddenError } from "@application/errors/ForbidenError";
+import type { IBookingRepository } from "@application/ports/repository/IBooking.repository";
+import type { IVedioCallUsecase } from "@application/ports/usecase/booking/IVedioCall.usecase";
+import { TYPES } from "@config/DI-container/TYPES";
+import { NotFoundError } from "@domain/errors/UserError";
+import { inject, injectable } from "inversify";
+@injectable()
+export class VedioCallUsecase implements IVedioCallUsecase {
+	constructor(
+		@inject(TYPES.BookingRepository)
+		private readonly _bookingRepository: IBookingRepository,
+	) {}
+
+	async execute(userId: string, bookingId: string): Promise<void> {
+		const booking = await this._bookingRepository.findById(bookingId);
+
+		if (!booking) {
+			throw new NotFoundError("booking not found");
+		}
+
+		if (booking.userId !== userId || booking.mentorId !== userId) {
+			throw new ForbiddenError("you dont have access to this request");
+		}
+	}
+}
